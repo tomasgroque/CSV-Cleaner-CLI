0880bd84c07a142d51b357aef6a66caf77688589
tomasgroque <tomas.groque@gmail.com>
2025-08-03 18:33:21 -0600

    Implement basic CSV file functions

--- CSV Cleaner CLI/index.py
+++ CSV Cleaner CLI/index.py
@@ -1,1 +1,11 @@
+import argparse
 import pandas as pd
+
+def import_csv(file_path):
+    return pd.read_csv(file_path)
+
+def clean_duplicates(df):
+    df.drop_duplicates()
+
+def strip_spaces(df):
+    return df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
